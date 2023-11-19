bff41a9e33d84cee736de5142efc0609f42dd71f
Dmitriy Onypko <vaporif@gmail.com>
2023-11-19 11:42:33 +0000

    add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
+  endpoints {
+    s3  = "http://localstack:4566"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "bucket"
+}
